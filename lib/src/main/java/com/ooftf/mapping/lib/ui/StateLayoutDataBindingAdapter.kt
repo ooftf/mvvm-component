786dc7e6c28a28a5e848c85c749152dffb86e840
ooftf <li994749769@gmail.com>
2020-10-23 18:14:22 +0800

    stateLayout添加分开的

--- lib/src/main/java/com/ooftf/mapping/lib/ui/StateLayoutDataBindingAdapter.kt
+++ lib/src/main/java/com/ooftf/mapping/lib/ui/StateLayoutDataBindingAdapter.kt
@@ -77,15 +77,15 @@ object StateLayoutDataBindingAdapter {
                         firstActionId?.let { setFirstActionId(it) }
                         secondActionId?.let { setSecondActionId(it) }
                         thirdActionId?.let { setThirdActionId(it) }
+                        setOnRetryListener {
+                            data.refresh()
+                        }
+                        setEmptyAction {
+                            data.emptyAction()
+                        }
+                        ViewHelper.replace(view, this, FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT))
                     }
                 }
-        ViewHelper.replace(view, slsView, FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT))
-        slsView.setOnRetryListener {
-            data.refresh()
-        }
-        slsView.setEmptyAction {
-            data.emptyAction()
-        }
         var observerState = view.getTag(R.id.observer_state)
         if (observerState == null) {
             observerState = Observer<Int> { integer: Int ->
@@ -96,5 +96,62 @@ object StateLayoutDataBindingAdapter {
                 data.getStateLayout().observe(it, observerState)
             }
         }
+    }
+
+    @JvmStatic
+    @BindingAdapter(value = ["stateLayout",
+        "errorAction", "emptyAction", "firstAction", "secondAction", "thirdAction",
+        "emptyLayoutId", "loadLayoutId", "errorLayoutId", "firstLayoutId", "secondLayoutId", "thirdLayoutId",
+        "emptyActionId", "errorActionId", "firstActionId", "secondActionId", "thirdActionId"], requireAll = false)
+    fun setStateLayoutUiMapping(view: View,
+                                stateLayout: Int?,
+                                errorAction: (() -> Unit)?,
+                                emptyAction: (() -> Unit)?,
+                                firstAction: (() -> Unit)?,
+                                secondAction: (() -> Unit)?,
+                                thirdAction: (() -> Unit)?,
+                                emptyLayoutId: Int?,
+                                loadLayoutId: Int?,
+                                errorLayoutId: Int?,
+                                firstLayoutId: Int?,
+                                secondLayoutId: Int?,
+                                thirdLayoutId: Int?,
+                                emptyActionId: Int?,
+                                errorActionId: Int?,
+                                firstActionId: Int?,
+                                secondActionId: Int?,
+                                thirdActionId: Int?) {
+        if (stateLayout == null) {
+            return
+        }
+
+        val slsView =
+                if (view.parent is StateLayoutSwitcher) {
+                    view.parent as StateLayoutSwitcher
+                } else {
+                    StateLayoutSwitcher(view.context).apply {
+                        setSuccessLayout(view)
+                        emptyLayoutId?.let { setEmptyLayoutId(it) }
+                        loadLayoutId?.let { setLoadLayoutId(it) }
+                        errorLayoutId?.let { setErrorLayoutId(it) }
+                        firstLayoutId?.let { setFirstLayoutId(it) }
+                        secondLayoutId?.let { setSecondLayoutId(it) }
+                        thirdLayoutId?.let { setThirdLayoutId(it) }
+
+                        emptyActionId?.let { setEmptyActionId(it) }
+                        errorActionId?.let { setRefreshActionId(it) }
+                        firstActionId?.let { setFirstActionId(it) }
+                        secondActionId?.let { setSecondActionId(it) }
+                        thirdActionId?.let { setThirdActionId(it) }
+
+                        errorAction?.let { setOnRetryListener(errorAction) }
+                        emptyAction?.let { setEmptyAction(errorAction) }
+                        firstAction?.let { setFirstAction(errorAction) }
+                        secondAction?.let { setSecondAction(errorAction) }
+                        thirdAction?.let { setThirdAction(errorAction) }
+                        ViewHelper.replace(view, this, FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT))
+                    }
+                }
+        StateLayoutSwitcher.setValue(slsView, stateLayout)
     }
 }
